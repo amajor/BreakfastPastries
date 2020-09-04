f9872f8a4cfde83bd1b4115ad53e3f35eba3bcd4
Alison Major <alison.major@gmail.com>
2020-09-03 21:13:32 -0400

    Create simulator
    Add simulator to see how it works.
    
    Wk1

---
+++ src/BreakfastSimulator.java
@@ -0,0 +1,16 @@
+public class BreakfastSimulator {
+	public static void main(String args[]) {
+		// Print out what we're doing!
+		System.out.println("It's time for breakfast! What is everyone having?");
+		
+		// Build Ewan's breakfast: Pancakes!
+		Pastry pastry1 = new Pancake();
+		System.out.println("\nEwan is having...");
+		System.out.println("  " + pastry1.getDescription());
+		
+		// Build Chris' breakfast: Waffles!
+		Pastry pastry2 = new Waffle();
+		System.out.println("\nChris is having...");
+		System.out.println("  " + pastry2.getDescription());
+	}
+}
